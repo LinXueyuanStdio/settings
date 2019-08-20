b493bec044e723dec522a21890d2ec8f64ef9a69
兮尘 <761516186@qq.com>
2019-08-20 23:17:36 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 #install oh-my-zsh
 echo "Installing oh-my-zsh ..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 
 # install pyenv
 pyenv_dir=$HOME/.pyenv
